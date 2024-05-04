dab5d37b7c9e31c40bcc33c34c0c568e9952ee7b
XevenLord <chuazonghong@gmail.com>
2024-05-05 04:21:24 +0800

    Add Jwt token things & progress until SecCfg

--- src/main/java/com/example/demo/cmo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/cmo/config/JwtAuthenticationFilter.java
@@ -1,11 +1,17 @@
 package com.example.demo.cmo.config;
 
+import com.example.demo.cmo.base.JwtSrv;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,6 +20,11 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtSrv jwtSrv;
+
+    private final UserDetailsService usrDtlsSrv;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -22,10 +33,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHdr = request.getHeader("Authorization");
         final String jwt;
+        final String usrEml;
         if (authHdr == null || !authHdr.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
         jwt = authHdr.substring(7);
+        usrEml = jwtSrv.extractUsrnm(jwt);
+        if (usrEml != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails usrDtls = this.usrDtlsSrv.loadUserByUsername(usrEml);
+            if (jwtSrv.isTokenValid(jwt, usrDtls)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        usrDtls,
+                        null,
+                        usrDtls.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
